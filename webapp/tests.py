d75b3336c8899d6fad7ffab25038f9a4049b7d16
takakd <13297326+takakd@users.noreply.github.com>
2021-07-14 00:07:09 +0900

    Fix: unit test

--- webapp/tests.py
+++ webapp/tests.py
@@ -8,7 +8,9 @@ class AppTestCase(unittest.TestCase):
    def test_root_text(self):
         tester = app.test_client(self)
         response = tester.get('/')
-        assert 'Hello world!' in response.data
+        # Fix: unit tests failed because of the type error
+        # assert 'Hello world!' in response.data
+        assert b'Hello world!' in response.data
 
 if __name__ == '__main__':
     unittest.main()
